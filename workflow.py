6fd1f303cd3fd098b2db3d5a72a847d803c662c6
ezwelty <ethan.welty@gmail.com>
2026-02-24 15:30:22 +0100

    Put all constants at top of workflow

--- workflow.py
+++ workflow.py
@@ -7,12 +7,51 @@ import amce.creation
 import amce.functions
 import amce.publish
 
-
-# ---- Constants ----
+# ---- Inputs ----
 
 INPUT_PATH: Path = Path('data/input')
+
+MASS_BALANCE_FILE: Path = INPUT_PATH / 'fog_bw-bs-ba.csv'
+ELEVATION_CHANGE_FILE: Path = INPUT_PATH / 'FOG_ELEVATION_CHANGE_DATA.csv'
+GLACIER_SERIES_FILE: Path = INPUT_PATH / 'FOG_GLACIER_SERIES.csv'
+REGIONAL_AREA_CHANGE_RATE_FILE: Path = INPUT_PATH / 'regional_area_change.csv'
+URUMQI_MISSING_YEARS_FILE: Path = INPUT_PATH / 'urumqi_missing_years.csv'
+RGI_AREA_FILE: Path = INPUT_PATH / '_RGI_All_ID_Area.csv'
+GLIMS_ATTRIBUTE_FILE: Path = INPUT_PATH / 'CAU_glims_attribute_table.csv'
+RGI_ATTRIBUTE_DIR: Path = INPUT_PATH / '00_rgi60/00_rgi60_attribs'
+GLACIER_ID_LUT_FILE: Path = INPUT_PATH / 'GLACIER_ID_LUT_links.csv'
+GLIMS_ATTRIBUTE_AREA_FILE: Path = INPUT_PATH / 'glims_CAU_attributes.csv'
+ZEMP_REGIONAL_SERIES_DIR: Path = INPUT_PATH / 'zemp_etal_regional_series'
+
+# ---- Outputs ----
+
 OUTPUT_PATH: Path = Path('data/output')
 OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
+
+GLACIER_COORDINATE_FILE: Path = OUTPUT_PATH / 'FOG_coord.csv'
+GEODETIC_CHANGE_FILE: Path = OUTPUT_PATH / '_FOG_GEO_MASS_BALANCE_DATA.csv'
+REGIONAL_AREA_FILE: Path = OUTPUT_PATH / 'regional_area.csv'
+BA_FILE: Path = OUTPUT_PATH / 'ba.csv'
+BA_UNC_FILE: Path = OUTPUT_PATH / 'ba_unc.csv'
+MEAN_ANOMALY_DIR: Path = OUTPUT_PATH / 'MEAN_spatial_gla_anom'
+LOOKUP_ANOMALY_DIR: Path = OUTPUT_PATH / 'LOOKUP_spatial_and_reg_ids'
+LONG_NORM_ANOMALY_DIR: Path = OUTPUT_PATH / 'LONG-NORM_spatial_gla_anom'
+REGION_OCE_DIR: Path = OUTPUT_PATH / 'OCE_files_by_region'
+REGIONAL_BALANCE_DIR: Path = OUTPUT_PATH / 'regional_balance'
+REGIONAL_BALANCE_ESSD_DIR: Path = OUTPUT_PATH / 'regional_balance_essd'
+MASS_LOSS_DIR: Path = OUTPUT_PATH / 'mass_loss'
+REGIONAL_TILE_DIR: Path = OUTPUT_PATH / 'Tiles_by_region_0.5'
+OCE_TILE_DIR: Path = OUTPUT_PATH / 'OCE_tiles_by_region_0.5'
+AREA_CHANGE_GRID_DIR: Path = OUTPUT_PATH / 'area_change_by_region_0.5'
+MASS_CHANGE_GRID_DIR: Path = OUTPUT_PATH / 'mass_change_by_region_0.5'
+GLOBAL_GRID_DIR: Path = OUTPUT_PATH / 'global_grid_0.5'
+GLOBAL_GRID_NETCDF_DIR: Path = OUTPUT_PATH / 'global_grid_netcdf_0.5'
+
+# ---- Parameters ----
+
+VERSION: str = '2026-02-10'
+
+INVESTIGATORS_TO_DROP: List[str] = ['Robert McNabb', 'Thorsten Seehaus']
 
 DENSITY_FACTOR: Tuple[float, float] = (0.85, 0.06)
 """Density of ice (mean, sigma) relative to water (1000 kg m-3)."""
@@ -23,102 +62,15 @@ BEGIN_YEAR: int = 1915
 # TODO: Determine from data
 END_YEAR: int = 2025
 
-# ---- 1. Glacier change data ----
-
-MASS_BALANCE_FILE: Path = INPUT_PATH / 'fog_bw-bs-ba.csv'
-
-amce.functions.format_mass_balance_data(
-    input_file=MASS_BALANCE_FILE,
-    begin_year=BEGIN_YEAR,
-    output_dir=OUTPUT_PATH,
-)
-
-# Input
-ELEVATION_CHANGE_FILE: Path = INPUT_PATH / 'FOG_ELEVATION_CHANGE_DATA.csv'
-GLACIER_SERIES_FILE: Path = INPUT_PATH / 'FOG_GLACIER_SERIES.csv'
-INVESTIGATORS_TO_DROP: List[str] = ['Robert McNabb', 'Thorsten Seehaus']
-
-# Output
-GLACIER_COORDINATE_FILE: Path = OUTPUT_PATH / 'FOG_coord.csv'
-GEODETIC_CHANGE_FILE: Path = OUTPUT_PATH / '_FOG_GEO_MASS_BALANCE_DATA.csv'
-
-amce.functions.format_elevation_change(
-    elevation_change_file=ELEVATION_CHANGE_FILE,
-    glacier_series_file=GLACIER_SERIES_FILE,
-    investigators_to_drop=INVESTIGATORS_TO_DROP,
-    glacier_coordinate_file=GLACIER_COORDINATE_FILE,
-    geodetic_change_file=GEODETIC_CHANGE_FILE,
-    density_factor=DENSITY_FACTOR
-)
-
-# Input
-REGIONAL_AREA_CHANGE_RATE_FILE: Path = INPUT_PATH / 'regional_area_change.csv'
-
-# OUTPUT
-REGIONAL_AREA_FILE: Path = OUTPUT_PATH / 'regional_area.csv'
-
-amce.functions.format_regional_area(
-    regional_area_change_rate_file=REGIONAL_AREA_CHANGE_RATE_FILE,
-    begin_year=BEGIN_YEAR,
-    end_year=END_YEAR,
-    regional_area_file=REGIONAL_AREA_FILE
-)
-
-# ---- 2. Kriging spatial anomalies ----
-
 YEAR_INI: int = 2011
 YEAR_FIN: int = 2020
+
 REGIONS: List[str] = ['ALA', 'WNA', 'ACN', 'ACS', 'GRL', 'ISL', 'SJM', 'SCA', 'RUA', 'ASN', 'CEU', 'CAU', 'ASC', 'ASW', 'ASE', 'TRP', 'NZL', 'ANT', 'SA1', 'SA2']
-
-# Input
-URUMQI_MISSING_YEARS_FILE: Path = INPUT_PATH / 'urumqi_missing_years.csv'
-
-# Output
-BA_FILE: Path = OUTPUT_PATH / 'ba.csv'
-BA_UNC_FILE: Path = OUTPUT_PATH / 'ba_unc.csv'
-MEAN_ANOMALY_DIR: Path = OUTPUT_PATH / 'MEAN_spatial_gla_anom'
-LOOKUP_ANOMALY_DIR: Path = OUTPUT_PATH / 'LOOKUP_spatial_and_reg_ids'
-LONG_NORM_ANOMALY_DIR: Path = OUTPUT_PATH / 'LONG-NORM_spatial_gla_anom'
-
-# TODO: Expose region configuration as parameter
-amce.functions.calculate_global_glacier_spatial_anomaly(
-    year_ini=YEAR_INI,
-    year_fin=YEAR_FIN,
-    begin_year=BEGIN_YEAR,
-    mass_balance_file=MASS_BALANCE_FILE,
-    ba_file=BA_FILE,
-    ba_unc_file=BA_UNC_FILE,
-    urumqi_missing_years_file=URUMQI_MISSING_YEARS_FILE,
-    glacier_coordinate_file=GLACIER_COORDINATE_FILE,
-    geodetic_change_file=GEODETIC_CHANGE_FILE,
-    regions=REGIONS,
-    mean_anomaly_dir=MEAN_ANOMALY_DIR,
-    lookup_anomaly_dir=LOOKUP_ANOMALY_DIR,
-    long_norm_anomaly_dir=LONG_NORM_ANOMALY_DIR
-)
-
-# ---- 3. Kriging global CE spatial anomaly ----
 
 MIN_YEAR_GEO_OBS: int = 0
 
 # NOTE: Hugonnet 5-year estimates are only dropped if min_length_geo >= 5
 MIN_LENGTH_GEO: float = 5.0
-
-# Output
-REGION_OCE_DIR: Path = OUTPUT_PATH / 'OCE_files_by_region'
-
-amce.functions.calculate_consensus_estimate_and_error_global_glacier_regional_anomaly(
-    begin_year=BEGIN_YEAR,
-    end_year=END_YEAR,
-    min_year_geo_obs=MIN_YEAR_GEO_OBS,
-    min_length_geo=MIN_LENGTH_GEO,
-    long_norm_anomaly_dir=LONG_NORM_ANOMALY_DIR,
-    geodetic_change_file=GEODETIC_CHANGE_FILE,
-    region_oce_dir=REGION_OCE_DIR,
-    regions=REGIONS
-)
-
-# ---- 4. Kriging regional mass balance ----
 
 # TODO: Merge with REGIONS and RGI_CODE and move to configuration
 RGI_REG: Dict[str, str] = {
@@ -168,79 +120,9 @@ RGI_CODE: Dict[str, str] = {
   'ANT' : '19'
 }
 
-# Input
-RGI_AREA_FILE: Path = INPUT_PATH / '_RGI_All_ID_Area.csv'
-GLIMS_ATTRIBUTE_FILE: Path = INPUT_PATH / 'CAU_glims_attribute_table.csv'
-RGI_ATTRIBUTE_DIR: Path = INPUT_PATH / '00_rgi60/00_rgi60_attribs'
-
-# Output
-REGIONAL_BALANCE_DIR: Path = OUTPUT_PATH / 'regional_balance'
-
-amce.functions.calculate_regional_mass_balance(
-    region_oce_dir=REGION_OCE_DIR,
-    regional_balance_dir=REGIONAL_BALANCE_DIR,
-    regions=REGIONS,
-    rgi_reg=RGI_REG,
-    rgi_code=RGI_CODE,
-    rgi_area_file=RGI_AREA_FILE,
-    glims_attribute_file=GLIMS_ATTRIBUTE_FILE,
-    rgi_attribute_dir=RGI_ATTRIBUTE_DIR
-)
-
-amce.functions.compile_regional_mass_balance(
-    regional_balance_dir=REGIONAL_BALANCE_DIR,
-    regions=REGIONS
-)
-
-# Input
-GLACIER_ID_LUT_FILE: Path = INPUT_PATH / 'GLACIER_ID_LUT_links.csv'
-GLIMS_ATTRIBUTE_AREA_FILE: Path = INPUT_PATH / 'glims_CAU_attributes.csv'
-
-# Output
-REGIONAL_BALANCE_ESSD_DIR: Path = OUTPUT_PATH / 'regional_balance_essd'
-
-amce.functions.calculate_regional_mass_balance_essd(
-    regional_balance_dir=REGIONAL_BALANCE_DIR,
-    rgi_code=RGI_CODE,
-    rgi_region=RGI_REG,
-    glacier_id_lut_file=GLACIER_ID_LUT_FILE,
-    glims_attribute_file=GLIMS_ATTRIBUTE_AREA_FILE,
-    rgi_attribute_dir=RGI_ATTRIBUTE_DIR,
-    regional_balance_essd_dir=REGIONAL_BALANCE_ESSD_DIR,
-    regions=REGIONS,
-    runs=['cal_series', 'error_dh', 'error_anom', 'error_rho', 'error_tot']
-)
-
-# ---- 5. Kriging regional mass loss ----
-
 # Period to calculate the cumulative mass loss
 INI_YR: int = 1976
 FIN_YR: int = 2025
-
-# Input
-ZEMP_REGIONAL_SERIES_DIR: Path = INPUT_PATH / 'zemp_etal_regional_series'
-
-# Output
-MASS_LOSS_DIR: Path = OUTPUT_PATH / 'mass_loss'
-
-amce.functions.calculate_regional_mass_loss(
-    regional_balance_dir=REGIONAL_BALANCE_DIR,
-    region_oce_dir=REGION_OCE_DIR,
-    regional_area_file=REGIONAL_AREA_FILE,
-    rgi_area_file=RGI_AREA_FILE,
-    zemp_regional_series_dir=ZEMP_REGIONAL_SERIES_DIR,
-    ini_yr=INI_YR,
-    fin_yr=FIN_YR,
-    regions=REGIONS,
-    rgi_code=RGI_CODE,
-    rgi_reg=RGI_REG,
-    mass_loss_dir=MASS_LOSS_DIR
-)
-
-#####################################
-# ---- Part 2: Creation workflow ----
-
-# ---- 0. Grid tiles per region ----
 
 RGI_REGION: Dict[str, str] = {
     'ACN' : 'ArcticCanadaNorth',
@@ -289,8 +171,116 @@ RGI_CODE: Dict[str, str] = {
 # TODO: Why is SAN used here instead of SA1 and SA2?
 REGIONS_SAN: List[str] = ['ALA', 'WNA', 'ACN', 'ACS', 'GRL', 'ISL', 'SJM', 'SCA', 'RUA', 'ASN', 'CEU', 'CAU', 'ASC', 'ASW', 'ASE', 'TRP', 'SAN', 'NZL', 'ANT']
 
-# Output
-REGIONAL_TILE_DIR: Path = OUTPUT_PATH / 'Tiles_by_region_0.5'
+YMIN: int = 1976
+YMAX: int = 2025
+
+
+# ---- 1. Glacier change data ----
+
+amce.functions.format_mass_balance_data(
+    input_file=MASS_BALANCE_FILE,
+    begin_year=BEGIN_YEAR,
+    output_dir=OUTPUT_PATH,
+)
+
+amce.functions.format_elevation_change(
+    elevation_change_file=ELEVATION_CHANGE_FILE,
+    glacier_series_file=GLACIER_SERIES_FILE,
+    investigators_to_drop=INVESTIGATORS_TO_DROP,
+    glacier_coordinate_file=GLACIER_COORDINATE_FILE,
+    geodetic_change_file=GEODETIC_CHANGE_FILE,
+    density_factor=DENSITY_FACTOR
+)
+
+amce.functions.format_regional_area(
+    regional_area_change_rate_file=REGIONAL_AREA_CHANGE_RATE_FILE,
+    begin_year=BEGIN_YEAR,
+    end_year=END_YEAR,
+    regional_area_file=REGIONAL_AREA_FILE
+)
+
+# ---- 2. Kriging spatial anomalies ----
+
+# TODO: Expose region configuration as parameter
+amce.functions.calculate_global_glacier_spatial_anomaly(
+    year_ini=YEAR_INI,
+    year_fin=YEAR_FIN,
+    begin_year=BEGIN_YEAR,
+    mass_balance_file=MASS_BALANCE_FILE,
+    ba_file=BA_FILE,
+    ba_unc_file=BA_UNC_FILE,
+    urumqi_missing_years_file=URUMQI_MISSING_YEARS_FILE,
+    glacier_coordinate_file=GLACIER_COORDINATE_FILE,
+    geodetic_change_file=GEODETIC_CHANGE_FILE,
+    regions=REGIONS,
+    mean_anomaly_dir=MEAN_ANOMALY_DIR,
+    lookup_anomaly_dir=LOOKUP_ANOMALY_DIR,
+    long_norm_anomaly_dir=LONG_NORM_ANOMALY_DIR
+)
+
+# ---- 3. Kriging global CE spatial anomaly ----
+
+amce.functions.calculate_consensus_estimate_and_error_global_glacier_regional_anomaly(
+    begin_year=BEGIN_YEAR,
+    end_year=END_YEAR,
+    min_year_geo_obs=MIN_YEAR_GEO_OBS,
+    min_length_geo=MIN_LENGTH_GEO,
+    long_norm_anomaly_dir=LONG_NORM_ANOMALY_DIR,
+    geodetic_change_file=GEODETIC_CHANGE_FILE,
+    region_oce_dir=REGION_OCE_DIR,
+    regions=REGIONS
+)
+
+# ---- 4. Kriging regional mass balance ----
+
+amce.functions.calculate_regional_mass_balance(
+    region_oce_dir=REGION_OCE_DIR,
+    regional_balance_dir=REGIONAL_BALANCE_DIR,
+    regions=REGIONS,
+    rgi_reg=RGI_REG,
+    rgi_code=RGI_CODE,
+    rgi_area_file=RGI_AREA_FILE,
+    glims_attribute_file=GLIMS_ATTRIBUTE_FILE,
+    rgi_attribute_dir=RGI_ATTRIBUTE_DIR
+)
+
+amce.functions.compile_regional_mass_balance(
+    regional_balance_dir=REGIONAL_BALANCE_DIR,
+    regions=REGIONS
+)
+
+amce.functions.calculate_regional_mass_balance_essd(
+    regional_balance_dir=REGIONAL_BALANCE_DIR,
+    rgi_code=RGI_CODE,
+    rgi_region=RGI_REG,
+    glacier_id_lut_file=GLACIER_ID_LUT_FILE,
+    glims_attribute_file=GLIMS_ATTRIBUTE_AREA_FILE,
+    rgi_attribute_dir=RGI_ATTRIBUTE_DIR,
+    regional_balance_essd_dir=REGIONAL_BALANCE_ESSD_DIR,
+    regions=REGIONS,
+    runs=['cal_series', 'error_dh', 'error_anom', 'error_rho', 'error_tot']
+)
+
+# ---- 5. Kriging regional mass loss ----
+
+amce.functions.calculate_regional_mass_loss(
+    regional_balance_dir=REGIONAL_BALANCE_DIR,
+    region_oce_dir=REGION_OCE_DIR,
+    regional_area_file=REGIONAL_AREA_FILE,
+    rgi_area_file=RGI_AREA_FILE,
+    zemp_regional_series_dir=ZEMP_REGIONAL_SERIES_DIR,
+    ini_yr=INI_YR,
+    fin_yr=FIN_YR,
+    regions=REGIONS,
+    rgi_code=RGI_CODE,
+    rgi_reg=RGI_REG,
+    mass_loss_dir=MASS_LOSS_DIR
+)
+
+#####################################
+# ---- Part 2: Creation workflow ----
+
+# ---- 0. Grid tiles per region ----
 
 amce.creation.grid_tiles_per_region(
     rgi_region=RGI_REGION,
@@ -303,12 +293,6 @@ amce.creation.grid_tiles_per_region(
 
 
 # ---- 2. OCE to tiles 0.5 grid per region ----
-
-YMIN: int = 1976
-YMAX: int = 2025
-
-# Output
-OCE_TILE_DIR: Path = OUTPUT_PATH / 'OCE_tiles_by_region_0.5'
 
 amce.creation.oce2tiles_05_grid_per_region(
     regions=REGIONS,
@@ -333,10 +317,6 @@ AREA_CHG_RATE: Dict[str, float] = (
   .to_dict()
 )
 
-# Output
-AREA_CHANGE_GRID_DIR: Path = OUTPUT_PATH / 'area_change_by_region_0.5'
-MASS_CHANGE_GRID_DIR: Path = OUTPUT_PATH / 'mass_change_by_region_0.5'
-
 amce.creation.areachange_grid_per_region(
     regions=REGIONS,
     area_ref_year=AREA_REF_YEAR,
@@ -351,9 +331,6 @@ amce.creation.areachange_grid_per_region(
 
 # ---- Tiles to global grid 0.5 ----
 
-# Output
-GLOBAL_GRID_DIR: Path = OUTPUT_PATH / 'global_grid_0.5'
-
 amce.creation.tiles_to_global_grid(
     ymin=YMIN,
     ymax=YMAX,
@@ -365,9 +342,6 @@ amce.creation.tiles_to_global_grid(
 
 # ---- CSV to NetCDF4 global grid 0.5 ----
 
-# Output
-GLOBAL_GRID_NETCDF_DIR: Path = OUTPUT_PATH / 'global_grid_netcdf_0.5'
-
 amce.creation.csv2netcdf4_globalGrid(
     ymin=YMIN,
     ymax=YMAX,
@@ -377,8 +351,6 @@ amce.creation.csv2netcdf4_globalGrid(
 
 #####################################
 # ---- Part 3: Publishing workflow --
-
-VERSION: str = '2026-02-10'
 
 amce.publish.build_doi_release(
     mass_loss_dir=MASS_LOSS_DIR,
