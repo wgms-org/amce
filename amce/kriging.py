8b0ff05b41bc16224fddf3ccb9d103a305852eb0
ezwelty <ethan.welty@gmail.com>
2026-03-30 22:50:48 +0200

    Remove deprecated coordinate transformation functions

--- amce/kriging.py
+++ amce/kriging.py
@@ -1,107 +1,9 @@
-# imported from step 2
-from __future__ import annotations
 import numpy as np
 import pandas as pd
-import pyproj
 from pykrige.ok import OrdinaryKriging
 
 import amce.propagation
 
-############################################################################
-# COORDINATE TRANSFORMATIONS (FOR SPEED COMPUTING DISTS DURING ERROR PROPAG)
-############################################################################
-
-def latlon_to_utm(lat: float, lon: float) -> str:
-    """
-    Get UTM zone for a given latitude and longitude coordinates.
-
-    :param lat: Latitude coordinate.
-    :param lon: Longitude coordinate.
-
-    :returns: UTM zone.
-    """
-
-    if not (
-        isinstance(lat, (float, np.floating, int, np.integer))
-        and isinstance(lon, (float, np.floating, int, np.integer))
-    ):
-        raise TypeError("Latitude and longitude must be floats or integers.")
-
-    if not -180 <= lon < 180:
-        raise ValueError("Longitude value is out of range [-180, 180[.")
-    if not -90 <= lat < 90:
-        raise ValueError("Latitude value is out of range [-90, 90[.")
-
-    # Get UTM zone from name string of crs info
-    utm_zone = pyproj.database.query_utm_crs_info(
-        "WGS 84", area_of_interest=pyproj.aoi.AreaOfInterest(lon, lat, lon, lat)
-    )[0].name.split(" ")[-1]
-
-    return str(utm_zone)
-
-
-def utm_to_epsg(utm: str) -> int:
-    """
-    Get EPSG code of UTM zone.
-
-    :param utm: UTM zone.
-
-    :return: EPSG of UTM zone.
-    """
-
-    if not isinstance(utm, str):
-        raise TypeError("UTM zone must be a str.")
-
-    # Whether UTM is passed as single or double digits, homogenize to single-digit
-    utm = str(int(utm[:-1])) + utm[-1].upper()
-
-    # Get corresponding EPSG
-    epsg = pyproj.CRS(f"WGS 84 / UTM Zone {utm}").to_epsg()
-
-    return int(epsg)
-
-
-def reproject_points(
-    points: list[list[float]] | list[float] | tuple[list[float], list[float]] | np.ndarray, in_crs: pyproj.CRS, out_crs: pyproj.CRS
-) -> tuple[list[float], list[float]]:
-    """
-    Reproject a set of point from input_crs to output_crs.
-
-    :param points: Input points to be reprojected. Must be of shape (2, N), i.e (x coords, y coords)
-    :param in_crs: Input CRS
-    :param out_crs: Output CRS
-
-    :returns: Reprojected points, of same shape as points.
-    """
-    assert np.shape(points)[0] == 2, "points must be of shape (2, N)"
-
-    x, y = points
-    transformer = pyproj.Transformer.from_crs(in_crs, out_crs)
-    xout, yout = transformer.transform(x, y)
-    return (xout, yout)
-
-
-def reproject_from_latlon(
-    points: list[list[float]] | tuple[list[float], list[float]] | np.ndarray, out_crs: pyproj.CRS, round_: int = 2
-) -> tuple[list[float], list[float]]:
-    """
-    Reproject a set of point from lat/lon to out_crs.
-
-    :param points: Input points to be reprojected. Must be of shape (2, N), i.e (x coords, y coords)
-    :param out_crs: Output CRS
-    :param round_: Output rounding. Default of 2 ensures cm accuracy
-
-    :returns: Reprojected points, of same shape as points.
-    """
-    crs_4326 = pyproj.CRS.from_epsg(4326)
-    proj_points = reproject_points(points, crs_4326, out_crs)
-    proj_points = np.round(proj_points, round_)
-    return proj_points
-
-
-##################################################
-# SPATIAL INTERPOLATION WITH CORRELATION = KRIGING
-##################################################
 
 def krige_ba_anom(xobs: np.ndarray, yobs: np.ndarray, ba_anom_obs: np.ndarray, xpred: np.ndarray, ypred: np.ndarray, var_anom: float) -> tuple[np.ndarray, np.ndarray]:
     """
@@ -163,6 +65,7 @@ def krige_ba_anom(xobs: np.ndarray, yobs: np.ndarray, ba_anom_obs: np.ndarray, x
 
         return ba_anom_pred, sig_anom_pred
 
+
 def wrapper_latlon_krige_ba_anom(df_obs: pd.DataFrame,
                                  df_pred: pd.DataFrame,
                                  var_anom: float) -> tuple[np.ndarray, np.ndarray]:
@@ -175,22 +78,18 @@ def wrapper_latlon_krige_ba_anom(df_obs: pd.DataFrame,
 
     :return: Annual mass balance anomalies of glacier to predict, Error (1-sigma) of predicted anomalies.
     """
-
-    # Get median latitude and longitude among all values
-    med_lat = np.median(df_obs.lat)
-    med_lon = np.median(df_obs.lon)
-
-    # Find the metric (UTM) system centered on these coordinates
-    utm_zone = latlon_to_utm(med_lat, med_lon)
-    epsg = utm_to_epsg(utm_zone)
-
     # Reproject latitude and longitude to easting/northing
-    easting, northing = reproject_from_latlon((df_obs.lat, df_obs.lon),
-                                              out_crs=pyproj.CRS.from_epsg(epsg))
-    easting_pred, northing_pred = reproject_from_latlon((df_pred.lat, df_pred.lon),
-                                              out_crs=pyproj.CRS.from_epsg(epsg))
-    # Extract anomalies
-    ba_anom = df_obs.ba_anom
-
-    return krige_ba_anom(xobs=easting, yobs=northing, ba_anom_obs=ba_anom, xpred=easting_pred, ypred=northing_pred, var_anom=var_anom)
-
+    easting, northing = amce.propagation.geographic_to_utm(
+        latitude=df_obs.lat, longitude=df_obs.lon
+    )
+    easting_pred, northing_pred = amce.propagation.geographic_to_utm(
+        latitude=df_pred.lat, longitude=df_pred.lon
+    )
+    return krige_ba_anom(
+        xobs=easting,
+        yobs=northing,
+        ba_anom_obs=df_obs.ba_anom,
+        xpred=easting_pred,
+        ypred=northing_pred,
+        var_anom=var_anom
+    )
